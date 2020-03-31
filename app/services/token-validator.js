86110aa904b4728bf3b0ad1821f669fc22841408
supunsandeeptha <sandeeptha.supun@gmail.com>
2020-03-31 18:54:23 +1100

    adding the comments

--- app/services/token-validator.js
+++ app/services/token-validator.js
@@ -4,25 +4,25 @@ const OAuth2 = google.auth.OAuth2;
 const moment = require("moment");
 
 const oauth2Client = new OAuth2(
-    "445805490759-1653aqat0lhlv5ao2v7eotdo5dpbj828.apps.googleusercontent.com",
-    "dBmHiKbXg7xrP0BTRZ-3t5Fz",
-    "http://localhost:3000/auth/google/callback",
+    "445805490759-1653aqat0lhlv",  // use your client id
+    "dBmHiKb", // use your client secret
+    "http://localhost:3000/auth/google/callback", // use your callback url
 );
 
 exports.checkToken = (req, res, next) => {
   console.log(req.user);
   if (!req.user) {
-    console.log("NO USER");
-   // console.log(req);
+    console.log("NO USER Granted for the session ");
     return next();
   }
-
+    // calculating the remaining time to expire the access token
   if (moment().subtract(req.user.google.expiry_date, "s").format("x") > -3000) {
     oauth2Client.setCredentials({
       access_token: req.user.google.token,
       refresh_token: req.user.google.refreshToken
     });
 
+    // getting a new access token and updating the database
     oauth2Client.refreshAccessToken(function(err, tokens) {
       if (err) return next(err);
 
