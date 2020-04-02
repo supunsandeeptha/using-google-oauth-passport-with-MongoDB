c2c91302f5040129e366d2ea4c4843bbf1a7dd91
supunsandeeptha <sandeeptha.supun@gmail.com>
2020-04-02 18:14:37 +1100

    moving the google auth credentials to a config file

---
+++ config/auth.credentials.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'googleauth':{
+        'clientId' : '',  // Your client ID
+        'clientSecret':'', // Your client secret
+        'callBackUrl':'' // Your callback URL
+    }
+}
